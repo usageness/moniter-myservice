0a003b36f831f8e3286d074e7490abe1f5c22c9f
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-02-24 16:48:07 +0900

    test: 서비스 모니터링 테스트 작성

--- backend/src/status/status.controller.spec.ts
+++ backend/src/status/status.controller.spec.ts
@@ -16,14 +16,41 @@ describe('StatusController', () => {
     statusController = app.get<StatusController>(StatusController);
   });
 
-  describe('root', () => {
+  describe('status test', () => {
     it('모니터링 상태 체크는 200을 반환해야 한다.', async () => {
       const result = Object.assign({ statusCode: 200 });
+
       jest
         .spyOn(statusService, 'getMonitorStatus')
         .mockImplementation(() => result);
 
       expect(await statusController.getMonitorStatus()).toStrictEqual(result);
+    });
+
+    it('서비스 상태 체크에 성공하면 200을 반환해야 한다.', async () => {
+      const result = Object.assign({ statusCode: 200 });
+      const input = 'thumbnail_generator';
+
+      jest
+        .spyOn(statusService, 'getServiceStatus')
+        .mockImplementation(() => result);
+
+      expect(await statusController.getServiceStatus(input)).toStrictEqual(
+        result,
+      );
+    });
+
+    it('서비스 상태 체크에 실패하면 -1을 반환해야 한다.', async () => {
+      const result = Object.assign({ statusCode: -1 });
+      const input = 'invalid_ID';
+
+      jest
+        .spyOn(statusService, 'getServiceStatus')
+        .mockImplementation(() => result);
+
+      expect(await statusController.getServiceStatus(input)).toStrictEqual(
+        result,
+      );
     });
   });
 });
