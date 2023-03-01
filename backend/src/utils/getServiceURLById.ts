0bc4e4df62203e426f594d332272456041bc85cd
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-03-01 16:18:29 +0900

    refactor: getServiceURLById 함수 리팩토링

--- backend/src/utils/getServiceURLById.ts
+++ backend/src/utils/getServiceURLById.ts
@@ -1,16 +1,16 @@
-const getServiceURLById = (id: string) => {
-  switch (id) {
-    case 'blog_vercel':
-      return 'https://blog-usageness.vercel.app/';
-    case 'blog_tistory':
-      return 'https://usage.tistory.com/';
-    case 'thumbnail_generator':
-      return 'https://free-thumbnail-generator.vercel.app/';
-    case 'cors_proxy_server':
-      return 'https://cors.teamcrazyperformance.com/';
-    default:
-      throw new Error('unknown arguments ID');
-  }
+const serviceList = {
+  blog_vercel: 'https://blog-usageness.vercel.app/',
+  blog_tistory: 'https://usage.tistory.com/',
+  thumbnail_generator: 'https://free-thumbnail-generator.vercel.app/',
+  cors_proxy_server: 'https://blog-usageness.vercel.app/',
+};
+
+const getServiceURLById = (id: string): string => {
+  const serviceURL = serviceList[id];
+
+  if (!serviceURL) throw new Error('unknown arguments ID');
+
+  return serviceURL;
 };
 
 export default getServiceURLById;
